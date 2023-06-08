cb63c8f87fcc46aa13de5c6648ff425ca88930e1
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-08 17:15:44 +0200

    add function to generate pw

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
+<?php
+
+$length = isset($_GET['length']) ? $_GET['length'] : '';
+
+function create_pw($length)
+{
+    $arr_alph_num = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?$?%^&*()_-+={[}]:;@#|\<,>.?/';
+    $password = "";
+
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $arr_alph_num[rand(0, strlen($arr_alph_num) - 1)];
+    }
+
+    return $password;
+}
+
+$password = create_pw($length);
+echo "Password Generata: " . $password;
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,7 +40,7 @@
         <h1>Scrivi da quanti caratteri vuoi che sia composta la tua password e premi invia</h1>
 
         <form class="row g-3 mt-3" action="" method="GET">
-            <input class="form-control" type="number" placeholder="Scrivi qui il numero" aria-label="default input example">
+            <input class="form-control" type="number" placeholder="Scrivi qui il numero" aria-label="default input example" name="length">
             <button type="submit" class="btn btn-primary col-1 mb-3 me-3">Invia</button>
             <button type="reset" class="btn btn-secondary col-1 mb-3">Cancella</button>
         </form>
